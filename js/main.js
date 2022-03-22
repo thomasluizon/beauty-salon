a9bbfead8a29a69622d1875f51f22de6904aa7d2
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-03-22 17:02:42 -0300

    added new things

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const drawer = document.querySelector('[data-menu]');
+const drawerToggleBtns = document.querySelectorAll('.toggle');
+const drawerItems = document.querySelectorAll('.menu__item');
+const header = document.querySelector('.header');
+const navHeight = header.offsetHeight;
+
+drawerToggleBtns.forEach(btn =>
+   btn.addEventListener('click', () => {
+      drawer.classList.toggle('show');
+   })
+);
+
+drawerItems.forEach(item =>
+   item.addEventListener('click', () => {
+      drawer.classList.remove('show');
+   })
+);
+
+window.addEventListener('scroll', () => {
+   if (window.scrollY >= navHeight) {
+      header.classList.add('scroll');
+   } else {
+      header.classList.remove('scroll');
+   }
+});
