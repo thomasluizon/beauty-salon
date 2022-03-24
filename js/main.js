0040a9718fb77daf6368951a9155cc187266f1bb
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-03-24 14:42:50 -0300

    final commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const drawerToggleBtns = document.querySelectorAll('.toggle');
 const drawerItems = document.querySelectorAll('.menu__item');
 const header = document.querySelector('.header');
 const navHeight = header.offsetHeight;
+const backToTopBtn = document.querySelector('.back-to-top');
 
 drawerToggleBtns.forEach(btn =>
    btn.addEventListener('click', () => {
@@ -16,14 +17,13 @@ drawerItems.forEach(item =>
    })
 );
 
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScrolling() {
    if (window.scrollY >= navHeight) {
       header.classList.add('scroll');
    } else {
       header.classList.remove('scroll');
    }
-});
-
+}
 // Swiper
 
 const swiper = new Swiper('.swiper', {
@@ -32,4 +32,27 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-pagination',
    },
    keyboard: true,
+   breakpoints: {
+      767: {
+         slidesPerView: 2,
+         setWrapperSize: true,
+      },
+   },
+});
+
+// back to top btn
+
+function backToTop() {
+   if (window.scrollY >= 560) {
+      backToTopBtn.classList.add('show');
+   } else {
+      backToTopBtn.classList.remove('show');
+   }
+}
+
+// When scrolling
+
+window.addEventListener('scroll', function () {
+   changeHeaderWhenScrolling();
+   backToTop();
 });
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Beauty Salon project
+
+![Design preview in fullscreen](./prints/fullscreen.png)
+
+![Design preview in mobile](./prints/mobile.png)
+
+Made using only HTML, CSS and JavaScript
